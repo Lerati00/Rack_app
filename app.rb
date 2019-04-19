997e07c978c86d58b8d7594c35b319c1c9291821
lerati <nuda1234@bk.ru>
2019-04-19 16:57:06 +0300

    rack interface

--- app.rb
+++ app.rb
@@ -1,24 +1,32 @@
+
+require_relative 'time_format_handler'
+
 class App
-     
   def call(env)
-    [status, headers, body]
+    @request = Rack::Request.new(env)
+    @response = Rack::Response.new
+
+    if @request.path_info == "/time"
+      time_response(TimeFormatHandler.new(@request.params["format"]))
+    else
+      compose_response(404, "Unknown resource '#{@request.path_info}'\n")
+    end
   end
 
   private
 
-  def perform_request
-    sleep rand(2..3)
+  def compose_response(status, text)
+    @response.status = status
+    @response.write "#{text}"
+    @response.headers['Content-Type'] = 'text/plain'
+    @response.finish
   end
 
-  def status
-    200
-  end
-
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-   
-  def body
-    ["Welcome aboard\n"]
+  def time_response(handler)
+    if handler.unknown_formats.empty?
+      compose_response(200, "#{handler.result}\n")
+    else
+      compose_response(400, "Unknown time format #{handler.unknown_formats}\n")
+    end
   end
 end
